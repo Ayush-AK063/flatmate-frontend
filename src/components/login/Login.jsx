2fb34d61cb077694914d343a369701088e540a12
AKmzp063 <akmirzapur063@gmail.com>
2025-09-07 21:54:04 +0530

    update

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import './login.css'
-import { Link, useNavigate } from 'react-router-dom'
-import { clearErrors, getLoginAction } from '../../store/action/userAction';
-import { useDispatch, useSelector } from 'react-redux';
-import toast from 'react-hot-toast';
-
+import React, { useEffect, useState, useRef } from "react";
+import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { clearErrors, getLoginAction } from "../../store/action/userAction";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
 
 const Login = () => {
   const navigateTo = useNavigate();
-  const { isAuthenticated , error, message ,loading} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { isAuthenticated, error, message, loading } = useSelector(
+    (state) => state.user
+  );
+
+  const toastId = useRef(null);
+
   useEffect(() => {
     if (isAuthenticated) {
       navigateTo("/");
@@ -16,29 +21,23 @@ const Login = () => {
 
     if (error === "You are not Login") {
       dispatch(clearErrors());
-    }else if(error){
+    } else if (error) {
       toast.error(error);
       dispatch(clearErrors());
     }
 
-    
     if (message) {
       toast.success(message);
       dispatch(clearErrors());
     }
 
-    let toastId = false
-    if (loading) {
-      toastId = toast.loading('Loading...');
-    }else{
-      toast.dismiss(toastId);
+    if (loading && !toastId.current) {
+      toastId.current = toast.loading("Loading...");
+    } else if (!loading && toastId.current) {
+      toast.dismiss(toastId.current);
+      toastId.current = null;
     }
-    
-
-  }, [isAuthenticated ,message ,error, loading, dispatch]);
-
-
-  const dispatch = useDispatch();
+  }, [isAuthenticated, message, error, loading, dispatch, navigateTo]);
 
   const [loginData, setLoginData] = useState({
     email: "",
@@ -54,29 +53,39 @@ const Login = () => {
   };
 
   const loginSubmit = (e) => {
-    
     e.preventDefault();
     dispatch(getLoginAction(loginData));
-    if (error) {
-      
-      toast.error(error);
-      dispatch(clearErrors());
-    }
   };
 
   return (
-<div className="main">
-  <div className="form">
-    <h2>Login Here</h2>
-    <input type="email" value={loginData.email} onChange={onLoginChange} name="email" placeholder="Enter Email Here" />
-    <input type="password" value={loginData.password} onChange={onLoginChange} name="password" placeholder="Enter Password Here" />
-    <button onClick={loginSubmit} className="btnn"><a href="#">Login</a></button>
-    <p className="link">Don't have an account<br />
-      <Link to={"/register"} >Sign up </Link></p>
-  </div>
-</div>
+    <div className="main">
+      <div className="form">
+        <h2>Login Here</h2>
+        <input
+          type="email"
+          value={loginData.email}
+          onChange={onLoginChange}
+          name="email"
+          placeholder="Enter Email Here"
+        />
+        <input
+          type="password"
+          value={loginData.password}
+          onChange={onLoginChange}
+          name="password"
+          placeholder="Enter Password Here"
+        />
+        <button onClick={loginSubmit} className="btnn">
+          Login
+        </button>
+        <p className="link">
+          Don't have an account?
+          <br />
+          <Link to="/register">Sign up</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
